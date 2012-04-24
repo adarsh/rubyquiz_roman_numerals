404ef2f9bfd5cfed7ed3a43c329527fba98cd405
Harlow Ward <harlow@apprentice.io>
2012-04-24 10:42:12 -0400

    use divmod for count and remainder

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,40 +1,29 @@
 #!/usr/bin/env ruby
 
 class RomanNumerals
-  ROMAN_ARABIC_MAP = {
-    'M'  => 1000,
-    'CM' => 900,
-    'D'  => 500,
-    'CD' => 400,
-    'C'  => 100,
-    'XC' => 90,
-    'L'  => 50,
-    'XL' => 40,
-    'X'  => 10,
-    'IX' => 9,
-    'V'  => 5,
-    'IV' => 4,
-    'I'  => 1 }
+  MAX_ROMAN = 3999
 
-  def initialize(n)
-    @arabic = n
+  def initialize(value)
+    if value <= 0 or value > MAX_ROMAN
+      fail "Roman values mus be > 0 and <= #{MAX_ROMAN}"
+    end
+    @value = value
   end
 
+  ROMAN_ARABIC_MAP = {
+    'M'  => 1000, 'CM' => 900, 'D'  => 500, 'CD' => 400,
+    'C'  =>  100, 'XC' =>  90, 'L'  =>  50, 'XL' =>  40,
+    'X'  =>   10, 'IX' =>   9, 'V'  =>   5, 'IV' =>   4,
+    'I'  =>    1 }
+
   def to_roman
-    roman_output = ""
-
-    ROMAN_ARABIC_MAP.map do |roman_numeral, arabic_numeral|
-      division_result_rounded = @arabic / arabic_numeral
-
-      if division_result_rounded > 0
-        roman_letters_added = roman_numeral * division_result_rounded
-        roman_output << roman_letters_added
-        decrement_remaining_arabic_value = division_result_rounded * arabic_numeral
-        @arabic -= decrement_remaining_arabic_value
-      end
+    value = @value
+    roman = ROMAN_ARABIC_MAP.collect do |code, factor|
+      count, value = value.divmod(factor)
+      code * count
     end
 
-    roman_output
+    roman.join('')
   end
 end
 
